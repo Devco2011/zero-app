01dc5e68b7c0034a0debba6f3c16462bace2677d
Devco2011 <devco2011@gmail.com>
2021-02-03 18:07:45 -0600

    setting up auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route } from 'react-router-dom';
 import { NavBar } from './components/zero/NavBar';
 import { ApplicationViews } from './components/ApplicationViews';
+import { FirebaseProvider } from './components/fbAuth/FirebaseProvider';
 
 function App() {
   return (
     <Route>
-      <ApplicationViews />
+      <FirebaseProvider>
+        <NavBar />
+        <ApplicationViews />
+      </FirebaseProvider>
     </Route>
   );
 }
--- src/components/zero/Header.js
+++ src/components/zero/Header.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState, useContext } from 'react';
+import { FirebaseContext } from "../fbAuth/FirebaseProvider";
+import { NavLink as RRNavLink } from "react-router-dom";
+import { Container, Row, Col, Button, NavItem } from 'react-bootstrap';
 
 export const Header = () => {
+    const { isLoggedIn, logout } = useContext(FirebaseContext);
+    const [expanded, setExpanded] = useState(false);
+
     return (
         <>
             <Container>
                 <Row>
                     <Col>
-                        <h2>This is the Header.</h2></Col>
+                        <h2>This is the new Header.</h2></Col>
+                    {isLoggedIn &&
+                        <>
+                            <Button className="nav-link text-right" variant="link" onClick={logout} style={{ border: "0" }}>Logout</Button>
+                        </>
+                    }
+                    {!isLoggedIn &&
+                        <>
+                            <NavItem className="nav-link text-right" variant="link"><RRNavLink to="/login">Login to Track Trash</RRNavLink></NavItem>
+                        </>
+                    }
                 </Row>
             </Container>
         </>
